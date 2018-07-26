43d02d64ed2b9f6d2f0cfc57fa73c59aecb0408d
lenderbook <41521807+lenderbook@users.noreply.github.com>
2018-07-26 01:34:36 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("DB_HOST", "nome_do_server_ou_ip");
+define("DB_DATABASE", "nome_database");
+define("DB_USER", "nome_usuario_do_banco");
+define("DB_PSW", "senha_de_acesso");
+define("UPLOAD_PATH", "caminho_para_o_apload_de_arquivos");
+
+
+
+?>
