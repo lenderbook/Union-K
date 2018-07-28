b6f363b3ab57b0fe58484bedd603a7b673a500af
lenderbook <41521807+lenderbook@users.noreply.github.com>
2018-07-27 22:42:19 -0300

    Add files via upload

--- javascript/editar.js
+++ javascript/editar.js
@@ -132,3 +132,27 @@ function delete_file(id_publicacao){
 
 }
 
+
+
+function excluir(){
+               
+       
+        document.getElementById("page-response").innerHTML=' Aguarde, estamos realizando o seu cadastro...';
+        xmlhttp.open("POST", "ajax/exclui_publicacao_cmd.php",true);
+        xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=iso-8859-1");
+        xmlhttp.onreadystatechange = function() {
+          if(xmlhttp.readyState == 4) { 
+          document.getElementById("page-response").innerHTML='';
+       // document.getElementById("page-response").innerHTML=xmlhttp.responseText;
+                   eval(xmlhttp.responseText);
+}
+};
+ 
+ campos ="cmd="+escape('excluir');
+  campos =campos+"&id_publicacao="+escape(document.getElementById("id_publicacao").value);
+ xmlhttp.send(campos);
+
+    
+    
+
+}
