2c2e04e34045dcbc69e9ac8055c46ebc3ddf03d6
lenderbook <41521807+lenderbook@users.noreply.github.com>
2018-07-27 22:42:55 -0300

    Add files via upload

--- ajax/exclui_publicacao_cmd.php
+++ ajax/exclui_publicacao_cmd.php
@@ -4,8 +4,8 @@ require_once '../database-class.php';
 
 if (!isset($_SESSION)) {session_start();}
 $id_usuario = $_SESSION['id_usuario'];
-if(isset($_SESSION['nivel'])){$nivel = $_SESSION['nivel'] ;}else{$nivel ='';}
 
+if(isset($_SESSION['nivel'])){$nivel = $_SESSION['nivel'] ;}else{$nivel ='';}
 
 
 if (isset($_POST['id_publicacao'])) {$id_publicacao =$_POST['id_publicacao'];}
@@ -15,10 +15,10 @@ $sql="SELECT  * from rede_publicacoes where id_publicacao = '".$id_publicacao."'
 $result = mysqli_query($conex->mysqli,$sql);
 $dados = $result->fetch_assoc();
 
-$id_usuario_publicacao = $dados['id_usuario'];
 $arquivo = $dados['arquivo'];
+$id_usuario_publicacao = $dados['id_usuario'];
 
-//Somente o dono da publicação pode alterá-la ou o admin
+//Somente o dono da publicação pode alterá-la
 if($id_usuario_publicacao != $id_usuario and $nivel =='1'){
 
 echo "document.getElementById('page-response').className ='warning-error';";
@@ -26,18 +26,27 @@ echo "document.getElementById('page-response').innerHTML='Ops, somente o usuário
 exit;
 }
 
+
+if ($arquivo!=''){
 $file = UPLOAD_PATH. $arquivo;
-
 unlink($file);
+}
 
-$sql="update rede_publicacoes  set arquivo ='', tamanho ='' where id_publicacao = '".$id_publicacao."'";
+$sql="delete from rede_publicacoes  where id_publicacao = '".$id_publicacao."'";
 mysqli_query($conex->mysqli,$sql);
 
 
 echo "document.getElementById('page-response').className ='warning-success';";
-echo "document.getElementById('page-response').innerHTML='Arquivo excluído com sucesso!';";
+echo "document.getElementById('page-response').innerHTML='Publicação excluída com sucesso!';";
 echo "document.getElementById('div-edita-arquivo').className ='hidden';";
 echo "document.getElementById('div-upload-arquivo').className ='';";
+echo "document.getElementById('id_publicacao').value='';";
+echo "document.getElementById('file_name').value='';";
+echo "document.getElementById('titulo').value='';";
+echo "document.getElementById('url').value='';";
+echo "document.getElementById('descricao').value='';";
+
+
 
 
 
